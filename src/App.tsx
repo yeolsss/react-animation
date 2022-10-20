f9a8c84d11f5653ec8f1a176d183101f31195b78
yeol <yeol8810@gmail.com>
2022-10-20 11:22:31 +0900

    dragConstraints 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion, Variants } from 'framer-motion';
+import { useRef } from 'react';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,15 +18,35 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const boxVariants: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: '100px' },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: '100px' },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="click"></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
