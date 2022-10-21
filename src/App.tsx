24550867d73fcd506fae19d13a69907cf179bff2
yeol <yeol8810@gmail.com>
2022-10-21 16:14:35 +0900

    layout, layoutId done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
-import { AnimatePresence, motion, Variants } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  padding: 0px 200px;
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -20,66 +21,21 @@ const Box = styled(motion.div)`
   align-items: center;
   font-size: 22px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  position: absolute;
-  top: 100px;
+`;
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: rgba(49, 178, 238, 0.664);
 `;
 
-interface ICustomProps {
-  back: boolean;
-}
-const box: Variants = {
-  initial: ({ back }: ICustomProps) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  animate: {
-    opacity: 1,
-    scale: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: ({ back }: ICustomProps) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.5,
-    },
-  }),
-};
-
 function App() {
-  const [showing, setShowing] = useState(1);
-  const [back, setBack] = useState(false);
-  const onNextShowing = () => {
-    setBack(false);
-    setShowing((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-
-  const onPrevShowing = () => {
-    setBack(true);
-    setShowing((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-
+  const [clicked, setClicked] = useState(true);
+  const onClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence mode="wait" custom={{ back }}>
-        <Box
-          custom={{ back }}
-          variants={box}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          key={showing}
-        >
-          {showing}
-        </Box>
-      </AnimatePresence>
-      <button onClick={onPrevShowing}>prev</button>
-      <button onClick={onNextShowing}>next</button>
+    <Wrapper onClick={onClicked}>
+      <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
+      <Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
     </Wrapper>
   );
 }
